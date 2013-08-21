a26ccabb55d9503a0fb5ad0c7ba82bea8dcb2f5a
jeff <songpa@gmail.com>
2013-08-21 16:19:23 -0400

    payflow support

---
+++ app/models/spree/gateway/payflow.rb
@@ -0,0 +1,11 @@
+module Spree
+  class Gateway::Payflow < Gateway
+	  preference :login, :string
+	  preference :password, :password
+	  preference :partner, :string
+
+    def provider_class
+		ActiveMerchant::Billing::PayflowGateway
+    end
+  end
+end
