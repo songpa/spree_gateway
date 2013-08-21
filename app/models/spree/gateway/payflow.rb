908f29a528b473149e10af6a58318bd5ef36ee8f
jeff <songpa@gmail.com>
2013-08-21 16:37:11 -0400

    model update

--- app/models/spree/gateway/payflow.rb
+++ app/models/spree/gateway/payflow.rb
@@ -1,8 +1,10 @@
 module Spree
 	class Gateway::Payflow < Gateway
-	  preference :login, :string
-	  preference :password, :password
-	  preference :partner, :string
+	  preference :preferred_login, :string
+	  preference :preferred_password, :password
+	  preference :preferred_partner, :string
+	  
+	  attr_accessible :preferred_login, :preferred_password, :preferred_partner
 
 	  def provider_class
 		ActiveMerchant::Billing::PayflowGateway
